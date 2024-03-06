4a594b55b672105d4236e21eb72c6d644e42147f
krishgoudar <156109748+krishgoudar@users.noreply.github.com>
2024-03-06 12:27:35 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,105 @@
+function CheckForWin3 (Row: number, Col: number, RedTrueYellowFalse: boolean) {
+    if (RedTrueYellowFalse == true) {
+        _true = 0
+        for (let index = 0; index <= 3; index++) {
+            if (_true == 0) {
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(Col + index, Row + index), Red)) {
+                    _true = 0
+                    console.log("true is 0")
+                } else {
+                    _true = 1
+                    console.log("true is 1")
+                }
+            }
+        }
+        if (_true == 0) {
+            game.gameOver(true)
+        }
+    } else {
+        _true = 0
+        for (let index2 = 0; index2 <= 3; index2++) {
+            if (_true == 0) {
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(Col + index2, Row + index2), Yellow)) {
+                    _true = 0
+                } else {
+                    _true = 1
+                }
+            }
+        }
+        if (_true == 0) {
+            game.gameOver(true)
+        }
+    }
+    if (RedTrueYellowFalse == true) {
+        _true = 0
+        for (let index3 = 0; index3 <= 3; index3++) {
+            if (_true == 0) {
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(Col - index3, Row + index3), Red)) {
+                    _true = 0
+                    console.log("true is 0")
+                } else {
+                    _true = 1
+                    console.log("true is 1")
+                }
+            }
+        }
+        if (_true == 0) {
+            game.gameOver(true)
+        }
+    } else {
+        _true = 0
+        for (let index4 = 0; index4 <= 3; index4++) {
+            if (_true == 0) {
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(Col - index4, Row + index4), Yellow)) {
+                    _true = 0
+                } else {
+                    _true = 1
+                }
+            }
+        }
+        if (_true == 0) {
+            game.gameOver(true)
+        }
+    }
+}
 scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    Red = img`
+        . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+        . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+        . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+        . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
+        . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+        . . . 2 2 2 2 2 2 2 2 2 2 . . . 
+        `
+    Yellow = img`
+        . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+        . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+        . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+        5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
+        . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+        . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+        . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+        `
+    Token.ay = 0
     if (sprite.image.equals(img`
         . . . 2 2 2 2 2 2 2 2 2 2 . . . 
         . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
@@ -17,48 +118,22 @@ scene.onHitWall(SpriteKind.Player, function (sprite, location) {
         . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
         . . . 2 2 2 2 2 2 2 2 2 2 . . . 
         `)) {
-        tiles.setTileAt(tiles.locationInDirection(location, CollisionDirection.Top), img`
-            . . . 2 2 2 2 2 2 2 2 2 2 . . . 
-            . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
-            . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
-            2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-            2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-            2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-            2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-            2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-            2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-            2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-            2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-            2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-            2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-            . 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . 
-            . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
-            . . . 2 2 2 2 2 2 2 2 2 2 . . . 
-            `)
-        tiles.setWallAt(tiles.locationInDirection(location, CollisionDirection.Top), true)
+        tiles.setWallAt(sprite.tilemapLocation(), true)
+        New_Turn(false)
+        tiles.setTileAt(sprite.tilemapLocation(), Red)
+        CheckForWin(sprite.tilemapLocation().row, sprite.tilemapLocation().column, true)
+        CheckForWin2(sprite.tilemapLocation().row, sprite.tilemapLocation().column, true)
+        CheckForWin3(sprite.tilemapLocation().row, sprite.tilemapLocation().column, true)
+        CheckForWin4(sprite.tilemapLocation().row, sprite.tilemapLocation().column, true)
     } else {
-        tiles.setTileAt(tiles.locationInDirection(location, CollisionDirection.Top), img`
-            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
-            . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-            . 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
-            . . 5 5 5 5 5 5 5 5 5 5 5 5 . . 
-            . . . 5 5 5 5 5 5 5 5 5 5 . . . 
-            `)
-        tiles.setWallAt(tiles.locationInDirection(location, CollisionDirection.Top), true)
+        tiles.setWallAt(sprite.tilemapLocation(), true)
+        New_Turn(Reds_Turn)
+        tiles.setTileAt(sprite.tilemapLocation(), Yellow)
+        CheckForWin(sprite.tilemapLocation().row, sprite.tilemapLocation().column, false)
+        CheckForWin2(sprite.tilemapLocation().row, sprite.tilemapLocation().column, false)
+        CheckForWin3(sprite.tilemapLocation().row, sprite.tilemapLocation().column, false)
+        CheckForWin4(sprite.tilemapLocation().row, sprite.tilemapLocation().column, true)
     }
-    Reds_Turn = !(Reds_Turn)
-    New_Turn(Reds_Turn)
 })
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Right_Most_Column >= tiles.locationXY(tiles.locationOfSprite(Token), tiles.XY.column)) {
@@ -70,6 +145,91 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         tiles.placeOnTile(Token, tiles.getTileLocation(tiles.locationXY(tiles.locationOfSprite(Token), tiles.XY.column) - 1, tiles.locationXY(tiles.locationOfSprite(Token), tiles.XY.row)))
     }
 })
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Left_Most_Column <= tiles.locationXY(tiles.locationOfSprite(Token), tiles.XY.column)) {
+        tiles.placeOnTile(Token, tiles.getTileLocation(tiles.locationXY(tiles.locationOfSprite(Token), tiles.XY.column) - 1, tiles.locationXY(tiles.locationOfSprite(Token), tiles.XY.row)))
+    }
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Right_Most_Column >= tiles.locationXY(tiles.locationOfSprite(Token), tiles.XY.column)) {
+        tiles.placeOnTile(Token, tiles.getTileLocation(tiles.locationXY(tiles.locationOfSprite(Token), tiles.XY.column) + 1, tiles.locationXY(tiles.locationOfSprite(Token), tiles.XY.row)))
+    }
+})
+function checkfortie () {
+    if (tiles.getTilesByType(assets.tile`myTile3`).length + tiles.getTilesByType(assets.tile`myTile1`).length == 42) {
+        game.splash("\"There is a tie")
+    }
+}
+function CheckForWin (Row: number, Col: number, RedTrueYellowFalse: boolean) {
+    if (RedTrueYellowFalse == true) {
+        _true = 0
+        for (let index5 = 0; index5 <= 3; index5++) {
+            if (_true == 0) {
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(Col, Row + index5), Red)) {
+                    _true = 0
+                } else {
+                    _true = 1
+                }
+            }
+        }
+        if (_true == 0) {
+            game.gameOver(true)
+        }
+    } else {
+        _true = 0
+        for (let index6 = 0; index6 <= 3; index6++) {
+            if (_true == 0) {
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(Col, Row + index6), Yellow)) {
+                    _true = 0
+                } else {
+                    _true = 1
+                }
+            }
+        }
+        if (_true == 0) {
+            game.gameOver(true)
+        }
+    }
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    Token.ay = 300
+})
+function CheckForWin4 (Row: number, Col: number, RedTrueYellowFalse: boolean) {
+    if (RedTrueYellowFalse == true) {
+        _true = 0
+        for (let index7 = 0; index7 <= 3; index7++) {
+            if (_true == 0) {
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(Col - index7, Row), Red)) {
+                    _true = 0
+                    console.log("true is 0")
+                } else {
+                    _true = 1
+                    console.log("true is 1")
+                }
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(Col - index7, Row), assets.tile`transparency16`)) {
+                    _true = 1
+                }
+            }
+        }
+        if (_true == 0) {
+            game.gameOver(true)
+        }
+    } else {
+        _true = 0
+        for (let index8 = 0; index8 <= 3; index8++) {
+            if (_true == 0) {
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(Col - index8, Row), Yellow)) {
+                    _true = 0
+                } else {
+                    _true = 1
+                }
+            }
+        }
+        if (_true == 0) {
+            game.gameOver(true)
+        }
+    }
+}
 function New_Turn (Red: boolean) {
     if (Red) {
         Token = sprites.create(img`
@@ -112,13 +272,69 @@ function New_Turn (Red: boolean) {
     }
     tiles.placeOnTile(Token, tiles.getTileLocation(4, 0))
 }
-controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-    Token.ay = 100
-})
-let Token: Sprite = null
+function CheckForWin2 (Row: number, Col: number, RedTrueYellowFalse: boolean) {
+    if (RedTrueYellowFalse == true) {
+        _true = 0
+        for (let index9 = 0; index9 <= 3; index9++) {
+            if (_true == 0) {
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(Col + index9, Row), Red)) {
+                    _true = 0
+                    console.log("true is 0")
+                } else {
+                    _true = 1
+                    console.log("true is 1")
+                }
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(Col + index9, Row), assets.tile`transparency16`)) {
+                    _true = 1
+                }
+            }
+        }
+        if (_true == 0) {
+            game.gameOver(true)
+        }
+    } else {
+        _true = 0
+        for (let index10 = 0; index10 <= 3; index10++) {
+            if (_true == 0) {
+                if (tiles.tileAtLocationEquals(tiles.getTileLocation(Col + index10, Row), Yellow)) {
+                    _true = 0
+                } else {
+                    _true = 1
+                }
+            }
+        }
+        if (_true == 0) {
+            game.gameOver(true)
+        }
+    }
+}
+let Yellow: Image = null
+let Red: Image = null
+let _true = 0
 let Reds_Turn = false
-let Right_Most_Column = 0
 let Left_Most_Column = 0
+let Right_Most_Column = 0
+let Token: Sprite = null
+scene.setBackgroundColor(15)
+tiles.setCurrentTilemap(tilemap`level1`)
+Reds_Turn = true
+New_Turn(Reds_Turn)
+Reds_Turn = false
+scene.setBackgroundColor(15)
+tiles.setCurrentTilemap(tilemap`level1`)
+Reds_Turn = true
+New_Turn(Reds_Turn)
+Reds_Turn = false
+scene.setBackgroundColor(15)
+tiles.setCurrentTilemap(tilemap`level1`)
+Reds_Turn = true
+New_Turn(Reds_Turn)
+Reds_Turn = false
+scene.setBackgroundColor(15)
+tiles.setCurrentTilemap(tilemap`level1`)
+Reds_Turn = true
+New_Turn(Reds_Turn)
+Reds_Turn = false
 scene.setBackgroundColor(15)
 tiles.setCurrentTilemap(tilemap`level1`)
 Left_Most_Column = 2
